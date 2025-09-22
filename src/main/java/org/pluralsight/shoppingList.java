1f1625cf6833a6ffa86644f1af9d43e1e0b9b84a
gurungshashak <gurungshashak@gmail.com>
2025-09-22 14:30:41 -0400

    push

---
+++ src/main/java/org/pluralsight/shoppingList.java
@@ -0,0 +1,7 @@
+package org.pluralsight;
+
+public class shoppingList {
+    public static void main(String[] args) {
+        System.out.println("apricot, apple, banana, orange, mango, pineapple, cherry, blueberry, pear, watermelon");
+    }
+}
--- src/test/java/com/pluralsight/shoppingList.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight;
-
-public class shoppingList {
-    public static void main(String[] args) {
-        System.out.println("apricot");
-    }
-}
